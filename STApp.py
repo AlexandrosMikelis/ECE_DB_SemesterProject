e7b30bd6bbba79698d7f5e66a368770dfcd25857
Alexandros Mikelis <62079690+AlexandrosMikelis@users.noreply.github.com>
2022-01-04 21:56:49 +0200

    Add files via upload

--- STApp.py
+++ STApp.py
@@ -6,7 +6,7 @@ import numpy as np
 import pandas as pd
 from PIL import Image
 
-DBPath = "Database/DB/LibNetwork.db"
+DBPath = "Database/LibNetwork.db"
 
 def ClearData(data):
     clearedData = []
